9d15cc5fb21af84709a403bf6ac15e559d5df435
Gaelle Kerforne <matheoalban@gmail.com>
2020-01-05 14:36:27 +0100

    ajout JS

---
+++ JdmBootstrap4/public/JS/boutontopbts4.js
@@ -0,0 +1,24 @@
+//js pour le btn top
+    <script type="text/javascript">
+        //Get the button:
+        monbutton = document.getElementById("monBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                monbutton.style.display = "block";
+            } else {
+                monbutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    </script>
